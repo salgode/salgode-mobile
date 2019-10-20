551f8923ae4d95eae7ab04bdb104b1272f3078a7
Franco Méndez <fnmendez@uc.cl>
2019-10-19 22:04:54 -0300

    chore: add mobile app base code
    Features
      Expo init
      Travis CI config
      ESLint config
      Useful scripts

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  parser: "babel-eslint",
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  env: {
+    es6: true,
+    jest: true,
+    node: true,
+    browser: true,
+  },
+  extends: [
+    "prettier",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-native/all"
+  ],
+  plugins: [
+    "prettier",
+    "react",
+    "react-native"
+  ],
+  "globals": {
+    "React": "readonly"
+  },
+  rules: {
+		"prettier/prettier": ["error", {
+      trailingComma: "es5",
+      semi: false,
+      singleQuote: true,
+    }],
+		"prefer-const": ["error", {
+        "destructuring": "any",
+        "ignoreReadBeforeAssign": false
+    }],
+		"no-var": "error",
+    "eqeqeq": ["error", "smart"],
+    "react-native/no-color-literals": "off",
+    "no-console": 1,
+    "react/display-name": 1,
+    "react/prop-types": 1,
+    "react-native/no-inline-styles": 1
+	}
+};
---
+++ utils/preferYarn.js
@@ -0,0 +1,2 @@
+if (process.env.npm_execpath.indexOf('yarn') === -1)
+  throw new Error('Prefer yarn: https://yarnpkg.com/en/docs/install')

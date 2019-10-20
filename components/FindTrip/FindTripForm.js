4aa520ea8d34e51422ea4356ce5131423ed291b3
Daniel Leal <dlleal@uc.cl>
2019-10-20 20:07:40 -0300

    FindTripForm now renders a prop named 'items' as possible locations

--- components/FindTrip/FindTripForm.js
+++ components/FindTrip/FindTripForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { StyleSheet } from 'react-native'
 import { Appearance } from 'react-native-appearance';
-import { Text, Form, Item, Input, Label, Button } from 'native-base'
+import { Text, Form, Item, Input, Label, Button, Picker, Icon } from 'native-base'
 import DateTimePicker from "react-native-modal-datetime-picker";
 import Layout from '../../constants/Layout'
 
@@ -14,10 +14,13 @@ class FindTripForm extends Component {
       chosenDate: this.getDateTimestamp(new Date()),
       // 15 minutes offfset
       chosenTime: this.getTimeTimestamp(new Date()) + (15 * 60 * 1000),
-      inputValidity: true,
+      startLocation: undefined,
+      inputValidity: false,
       isDatePickerVisible: false,
       isTimePickerVisible: false,
     }
+
+    this.handleStartLocationPicked = this.handleStartLocationPicked.bind(this)
 
     this.showDatePicker = this.showDatePicker.bind(this)
     this.hideDatePicker = this.hideDatePicker.bind(this)
@@ -28,13 +31,10 @@ class FindTripForm extends Component {
     this.handleTimePicked = this.handleTimePicked.bind(this)
   }
 
-  showDatePicker() {
-    this.setState({ isDatePickerVisible: true });
-  };
-
-  hideDatePicker() {
-    this.setState({ isDatePickerVisible: false });
-  };
+  handleStartLocationPicked(startLocation) {
+    const validity =  startLocation != undefined;
+    this.setState({ startLocation: startLocation, inputValidity: !!validity });
+  }
 
   handleDatePicked(date) {
     dateTimestamp = this.getDateTimestamp(date);
@@ -58,6 +58,16 @@ class FindTripForm extends Component {
     const validity = this.state.chosenDate + timeTimestamp >= now.getTime();
     this.setState({ chosenTime: timeTimestamp, inputValidity: !!validity });
     this.hideTimePicker();
+  };
+
+  // HELPERS
+
+  showDatePicker() {
+    this.setState({ isDatePickerVisible: true });
+  };
+
+  hideDatePicker() {
+    this.setState({ isDatePickerVisible: false });
   };
 
   getDateTimestamp(date) {
@@ -109,23 +119,37 @@ class FindTripForm extends Component {
     return day.toString() + "/" + month.toString() + "/" + year.toString();
   }
 
+  // RENDERER
 
   render() {
     return (
       <Form style={styles.form}>
-        <Item inlineLabel regular style={styles.item}>
-          <Label style={styles.label}>DE</Label>
-          <Input
+        <Item inlineLabel regular picker style={styles.item}>
+          <Picker
+            mode="dropdown"
+            iosIcon={<Icon name="arrow-down" />}
             style={styles.input}
-            onChangeText={this.onChangePhoneNumber}
-            value={this.state.phoneNumber}
-          />
+            placeholder="Seleccionar punto de partida"
+            placeholderStyle={styles.label}
+            placeholderIconColor="#007aff"
+            selectedValue={this.state.startLocation}
+            onValueChange={this.handleStartLocationPicked}
+          >
+            {
+              this.props.items.map(item => (
+                <Picker.Item
+                  key={item["id"]}
+                  value={item["id"]}
+                  label={item["name"]}
+                />
+              ))
+            }
+          </Picker>
         </Item>
         <Item inlineLabel regular style={styles.item} onPress={this.showDatePicker}>
           <Label style={styles.label}>DÍA</Label>
           <Text style={styles.input}>{this.showDate(this.state.chosenDate)}</Text>
           <Input
-            style={styles.input}
             editable={false}
             value={this.showDate(this.state.chosenDate)}
             isVisible={false}
@@ -143,7 +167,6 @@ class FindTripForm extends Component {
           <Label style={styles.label}>HORA</Label>
           <Text style={styles.input}>{this.showTime(this.state.chosenTime)}</Text>
           <Input
-            style={styles.input}
             editable={false}
             value={this.showTime(this.state.chosenTime)}
             isVisible={false}

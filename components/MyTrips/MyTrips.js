9158858e94fd13e7169bf7976578c8ca4b50f616
Benjamin Earle <benjaminearle123@gmail.com>
2019-10-20 13:58:17 -0300

    Added Flatlist to trip list

--- components/MyTrips/MyTrips.js
+++ components/MyTrips/MyTrips.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { FlatList, SafeAreaView } from 'react-native'
 import PropTypes from 'prop-types'
 import Trip from './Trip/Trip'
 
 class MyTrips extends Component {
   render() {
     return (
-      <View>
-        {this.props.trips.map((trip, i) => (
-          <Trip
-            timestamp={trip.timestamp}
-            spacesAvailable={trip.spacesAvailable}
-            key={i}
-          />
-        ))}
-        <Trip
-          timestamp={Date.now()}
-          spacesAvailable={Math.floor(4 * Math.random())}
+      <SafeAreaView>
+        <FlatList
+          data={this.props.trips}
+          renderItem={({ item }) => (
+            <Trip
+              timestamp={item.timestamp}
+              spacesAvailable={item.spacesAvailable}
+            />
+          )}
         />
-      </View>
+      </SafeAreaView>
     )
   }
 }
--- screens/MyTripsScreen.js
+++ screens/MyTripsScreen.js
@@ -10,7 +10,55 @@ class MyTripsScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <MyTrips />
+        <MyTrips
+          trips={[
+            {
+              timestamp: 1571590002,
+              spacesAvailable: 3,
+              key: '0',
+            },
+            {
+              timestamp: 1571503602,
+              spacesAvailable: 3,
+              key: '1',
+            },
+            {
+              timestamp: 1571586402,
+              spacesAvailable: 3,
+              key: '2',
+            },
+            {
+              timestamp: 1570985202,
+              spacesAvailable: 3,
+              key: '3',
+            },
+            {
+              timestamp: 1571593602,
+              spacesAvailable: 3,
+              key: '4',
+            },
+            {
+              timestamp: 1571676402,
+              spacesAvailable: 3,
+              key: '5',
+            },
+            {
+              timestamp: 1572194802,
+              spacesAvailable: 3,
+              key: '6',
+            },
+            {
+              timestamp: 1571590002,
+              spacesAvailable: 3,
+              key: '7',
+            },
+            {
+              timestamp: 1571590002,
+              spacesAvailable: 3,
+              key: '8',
+            },
+          ]}
+        />
       </View>
     )
   }

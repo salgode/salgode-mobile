1922669b1ff50c7379a2ac1a6222068b83c16833
Benjamin Earle <30664457+MrEarle@users.noreply.github.com>
2019-10-20 20:56:51 -0300

    Connect login to backend (#5)
    * Connected to backend
    
    * Store user in redux state on login

---
+++ constants/BackendUrls.js
@@ -0,0 +1,7 @@
+const backendUrl =
+  'https://7wsx5vxfbi.execute-api.us-east-1.amazonaws.com/staging'
+
+export default {
+  backend: backendUrl,
+  login: `${backendUrl}/users/login`,
+}
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
 import { Text, StyleSheet, KeyboardAvoidingView } from 'react-native'
+import { connect } from 'react-redux'
+import { Spinner } from 'native-base'
 import PropTypes from 'prop-types'
 import LoginForm from '../components/Login/LoginForm'
+import { loginAsync } from '../utils/login'
+import { setUser } from '../redux/actions/user'
 
 class LoginScreen extends Component {
   static navigationOptions = {
@@ -10,25 +14,41 @@ class LoginScreen extends Component {
 
   constructor(props) {
     super(props)
-
-    this.onFormSend = this.onFormSend.bind(this)
+    this.state = {
+      loading: false,
+    }
+    this.onSend = this.onSend.bind(this)
   }
 
-  onFormSend() {
-    this.props.navigation.navigate('Home')
+  async onSend(email, password) {
+    this.setState({ loading: true })
+    const user = await loginAsync(email, password)
+    this.setState({ loading: false })
+    if (!user) {
+      alert('Hubo un problema iniciando sesión. Por favor intentalo de nuevo.')
+    } else if (!user.email) {
+      alert(
+        'Las credenciales ingresadas son inválidas. Por favor intentalo de nuevo.'
+      )
+    } else {
+      this.props.setUser(user)
+      this.props.navigation.navigate('Home')
+    }
   }
 
   render() {
     return (
       <KeyboardAvoidingView style={styles.container} behavior="height" enabled>
         <Text style={styles.title}>#Salgode</Text>
-        <LoginForm onSend={this.onFormSend} />
+        <LoginForm onSend={this.onSend} />
+        {this.state.loading && <Spinner />}
       </KeyboardAvoidingView>
     )
   }
 }
 
 LoginScreen.propTypes = {
+  setUser: PropTypes.func.isRequired,
   navigation: PropTypes.shape({
     navigate: PropTypes.func.isRequired,
   }).isRequired,
@@ -47,4 +67,11 @@ const styles = StyleSheet.create({
   },
 })
 
-export default LoginScreen
+const mapDispatchToProps = dispatch => ({
+  setUser: user => dispatch(setUser(user)),
+})
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(LoginScreen)

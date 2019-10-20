730e0dbc78b648b98de2c8c1776c77708c381d75
Benjamin Earle <30664457+MrEarle@users.noreply.github.com>
2019-10-20 17:57:49 -0300

    My trips (#3)
    * My Trips Layout
    
    * Refactored my trips
    
    * Tab for past and future trips
    
    * On press for add person to trip
    
    * Reactivated phone validation
    
    * Added Flatlist to trip list
    
    * Refactored to Trips
    
    * Refactoring repeated Icon + Text
    
    * Updated item key to tripId

---
+++ components/Trips/Trips.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { FlatList, SafeAreaView } from 'react-native'
+import PropTypes from 'prop-types'
+import MyTrip from './Trip/Trip'
+import RequestedTrip from './Trip/RequestedTrip'
+
+class MyTrips extends Component {
+  constructor(props) {
+    super(props)
+    this.Trip = this.props.isRequestedTrips ? RequestedTrip : MyTrip
+  }
+
+  render() {
+    const Trip = this.Trip
+    return (
+      <SafeAreaView>
+        <FlatList
+          data={this.props.trips}
+          renderItem={({ item }) => (
+            <Trip
+              timestamp={item.timestamp}
+              spacesUsed={item.spacesUsed}
+              user={item.user}
+              status={item.status}
+            />
+          )}
+          keyExtractor={item => item.tripId}
+        />
+      </SafeAreaView>
+    )
+  }
+}
+
+MyTrips.propTypes = {
+  trips: PropTypes.array,
+  isRequestedTrips: PropTypes.bool,
+}
+
+MyTrips.defaultProps = {
+  trips: [],
+  isRequestedTrips: false,
+}
+
+export default MyTrips

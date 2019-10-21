6831ba6a732f7ae80e3d2967c5d7bd7b6191092b
Benjamin Earle <benjaminearle123@gmail.com>
2019-10-21 14:37:53 -0300

    Fixed navigation order

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -10,7 +10,7 @@ export default createAppContainer(
   createSwitchNavigator({
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-    Main: MainTabNavigator,
     LoginStack: LoginNavigator,
+    Main: MainTabNavigator,
   })
 )

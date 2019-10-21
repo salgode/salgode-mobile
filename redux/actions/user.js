59325e2e59c58747f063f84c935cc9e77393ea40
Matias Andrade <mandrade2@uc.cl>
2019-10-21 03:28:25 -0300

    feat: redux action and reducer

--- redux/actions/user.js
+++ redux/actions/user.js
@@ -2,6 +2,9 @@ export const actions = {
   USER_LOGIN: 'USER/LOGIN',
   USER_LOGIN_FAIL: 'USER/LOGIN_FAIL',
   USER_LOGIN_SUCESS: 'USER/LOGIN_SUCCESS',
+  USER_SIGNUP: 'USER/SIGNUP',
+  USER_SIGNUP_FAIL: 'USER/SIGNUP_FAIL',
+  USER_SIGNUP_SUCESS: 'USER/SIGNUP_SUCCESS',
 }
 
 export function loginUser(email, password) {
@@ -14,6 +17,49 @@ export function loginUser(email, password) {
         data: {
           email,
           password,
+        },
+      },
+    },
+  }
+}
+
+export function signupUser(
+  name,
+  lastName,
+  email,
+  phone,
+  password,
+  passwordRepeat
+  // selfieLink,
+  // driverLicenseLink,
+  // dniLink,
+  // carPlate,
+  // carColor,
+  // carBrand,
+  // carModel
+) {
+  return {
+    type: actions.USER_SIGNUP,
+    payload: {
+      request: {
+        url: `/users`,
+        method: 'post',
+        data: {
+          name,
+          lastName,
+          email,
+          phone,
+          password,
+          passwordRepeat,
+          // selfieLink,
+          // driverLicenseLink,
+          // dniLink,
+          // car: {
+          //   carPlate,
+          //   carColor,
+          //   carBrand,
+          //   carModel,
+          // },
         },
       },
     },
--- redux/reducers/user.js
+++ redux/reducers/user.js
@@ -1,19 +1,26 @@
 import { actions as userActions } from '../actions/user'
 
 export default function userReducer(state = {}, action) {
-  // console.log('before', action)
   switch (action.type) {
     case userActions.USER_LOGIN:
       return { ...state, loading: true }
     case userActions.USER_LOGIN_SUCCESS:
-      // console.log('after', action)
-
       return { ...state, loading: false, user: action.payload.data }
     case userActions.USER_LOGIN_FAIL:
       return {
         ...state,
         loading: false,
         error: 'Error while login user',
+      }
+    case userActions.USER_SIGNUP:
+      return { ...state, loading: true }
+    case userActions.USER_SIGNUP_SUCCESS:
+      return { ...state, loading: false, user: action.payload.data }
+    case userActions.USER_SIGNUP_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: 'Error while signup user',
       }
     default:
       return state

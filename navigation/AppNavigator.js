cfb9e41d2b415fa8d60b75eb548ed9a4922fcc14
Diego Silva <disilva2@uc.cl>
2019-10-21 05:32:48 -0300

    fixing minor issue

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -4,13 +4,12 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation'
 
 import MainTabNavigator from './MainTabNavigator'
 import LoginScreen from '../screens/LoginScreen'
-import CreateTripScreen from '../screens/CreateTripScreen';
 
 export default createAppContainer(
   createSwitchNavigator({
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-    Login: CreateTripScreen,
+    Login: LoginScreen,
     Main: MainTabNavigator,
   })
 )

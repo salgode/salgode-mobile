158d1889e2f6d22868f9711a20da0887b1428aa1
Benjamin Earle <benjaminearle123@gmail.com>
2019-10-21 11:04:19 -0300

    Added trips store

--- redux/store.js
+++ redux/store.js
@@ -2,7 +2,9 @@ import { applyMiddleware, combineReducers, createStore } from 'redux'
 import axiosMiddleware from 'redux-axios-middleware'
 import axios from 'axios'
 import userReducer from './reducers/user'
+import futureTripReducer from './reducers/trips'
 import { userModel } from './models/user'
+import { futureTripsModel } from './models/trips'
 
 const client = axios.create({
   baseURL: 'https://7wsx5vxfbi.execute-api.us-east-1.amazonaws.com/staging',
@@ -12,11 +14,12 @@ const client = axios.create({
 
 const reducer = combineReducers({
   user: userReducer,
+  futureTrips: futureTripReducer,
   loading: false,
 })
 
 export const store = createStore(
   reducer,
-  { user: userModel },
+  { user: userModel, futureTrips: futureTripsModel },
   applyMiddleware(axiosMiddleware(client))
 )

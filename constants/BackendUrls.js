2b9815de5d1913a5aea9c4da46cbaf6bac703d22
Matías Andrade Guzmán <mandrade2@uc.cl>
2019-10-21 01:36:43 -0300

    Fix/login action (#10)
    * feat: login as a redux action and reducer
    
    * refactor: cleanup
    
    * fix: alerts

--- constants/BackendUrls.js
+++
@@ -1,7 +0,0 @@
-const backendUrl =
-  'https://7wsx5vxfbi.execute-api.us-east-1.amazonaws.com/staging'
-
-export default {
-  backend: backendUrl,
-  login: `${backendUrl}/users/login`,
-}
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, KeyboardAvoidingView } from 'react-native'
+import { Text, StyleSheet, KeyboardAvoidingView, Alert } from 'react-native'
 import { connect } from 'react-redux'
 import { Spinner } from 'native-base'
 import PropTypes from 'prop-types'
 import LoginForm from '../components/Login/LoginForm'
-import { loginAsync } from '../utils/login'
-import { setUser } from '../redux/actions/user'
+import { loginUser } from '../redux/actions/user'
 
 class LoginScreen extends Component {
   static navigationOptions = {
@@ -22,18 +21,22 @@ class LoginScreen extends Component {
 
   async onSend(email, password) {
     this.setState({ loading: true })
-    const user = await loginAsync(email, password)
-    this.setState({ loading: false })
+
+    const user = await this.props.login(email, password).then(response => {
+      return response.payload.data
+    })
     if (!user) {
-      alert('Hubo un problema iniciando sesión. Por favor intentalo de nuevo.')
+      Alert.alert(
+        'Hubo un problema iniciando sesión. Por favor intentalo de nuevo.'
+      )
     } else if (!user.email) {
-      alert(
+      Alert.alert(
         'Las credenciales ingresadas son inválidas. Por favor intentalo de nuevo.'
       )
     } else {
-      this.props.setUser(user)
       this.props.navigation.navigate('Home')
     }
+    await this.setState({ loading: false })
   }
 
   render() {
@@ -48,7 +51,7 @@ class LoginScreen extends Component {
 }
 
 LoginScreen.propTypes = {
-  setUser: PropTypes.func.isRequired,
+  login: PropTypes.func.isRequired,
   navigation: PropTypes.shape({
     navigate: PropTypes.func.isRequired,
   }).isRequired,
@@ -68,7 +71,7 @@ const styles = StyleSheet.create({
 })
 
 const mapDispatchToProps = dispatch => ({
-  setUser: user => dispatch(setUser(user)),
+  login: (email, password) => dispatch(loginUser(email, password)),
 })
 
 export default connect(

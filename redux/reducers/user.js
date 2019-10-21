daf975acef57d13b8074ff25d6e751ad334e9b9e
Benjamin Earle <30664457+MrEarle@users.noreply.github.com>
2019-10-21 16:25:19 -0300

    Fix/login and signup (#32)
    * login and signup fix
    
    * Fixed Login and Signup

--- redux/reducers/user.js
+++ redux/reducers/user.js
@@ -1,11 +1,12 @@
 import { actions as userActions } from '../actions/user'
 
 export default function userReducer(state = {}, action) {
-  // console.log('before', action)
+  console.log('before', action)
   switch (action.type) {
     case userActions.USER_LOGIN:
       return { ...state, loading: true }
     case userActions.USER_LOGIN_SUCCESS:
+      console.log('after', action)
       return { ...state, loading: false, user: action.payload.data }
     case userActions.USER_LOGIN_FAIL:
       return {

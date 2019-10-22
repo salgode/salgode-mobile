84d497dbc2a0f64c65afdcc42bad06662f152259
Benjamin Earle <30664457+MrEarle@users.noreply.github.com>
2019-10-21 21:34:59 -0300

    Update AppNavigator.js

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -11,7 +11,6 @@ export default createAppContainer(
   createSwitchNavigator({
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-    ResolveAuth: ResolveAuthScreen,
     LoginStack: LoginNavigator,
     Main: MainTabNavigator,
   })

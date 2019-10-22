22f9e85b90f19cbbcc1f53d1449df4fff8dc9f0b
Benjamin Earle <benjaminearle123@gmail.com>
2019-10-22 19:45:30 -0300

    Fixed unhandled promise

--- components/Trips/Trip/ChooseTrip.js
+++ components/Trips/Trip/ChooseTrip.js
@@ -25,7 +25,7 @@ const ChooseTrip = ({
   const parseStops = async () => {
     const stps = await getTripInfo(tripId, token)
     _setStops(stps.trip_route_points)
-    setStops(stps.trip_route_points.map(s => s.address))
+    setStops(stps.trip_route_points.map(s => (s || { address: '' }).address))
   }
 
   React.useEffect(() => {
@@ -36,34 +36,34 @@ const ChooseTrip = ({
   return loading ? (
     <View></View>
   ) : (
-      <Card style={styles.containerRequested}>
-        <CardItem style={styles.dataContainer}>
-          <View style={styles.user}>
-            <View style={styles.userData}>
-              <Ionicons
-                name={Platform.OS === 'ios' ? 'ios-contact' : 'md-contact'}
-                size={80}
-              />
-              <Text style={styles.userText}>{user.name}</Text>
-            </View>
-            <View style={styles.iconInfoGroup}>
-              <View style={styles.iconContainer}>
-                <Icon name="like1" style={styles.infoIcon} />
-                <Text style={styles.iconText}>{user.reputation}</Text>
-              </View>
+    <Card style={styles.containerRequested}>
+      <CardItem style={styles.dataContainer}>
+        <View style={styles.user}>
+          <View style={styles.userData}>
+            <Ionicons
+              name={Platform.OS === 'ios' ? 'ios-contact' : 'md-contact'}
+              size={80}
+            />
+            <Text style={styles.userText}>{user.name}</Text>
+          </View>
+          <View style={styles.iconInfoGroup}>
+            <View style={styles.iconContainer}>
+              <Icon name="like1" style={styles.infoIcon} />
+              <Text style={styles.iconText}>{user.reputation}</Text>
             </View>
           </View>
-        </CardItem>
-        <CardItem style={styles.locationContainer}>
-          <Location color={'#0000FF'} location={stops[0]} />
-          <Location color={'#33C534'} location={stops[stops.length - 1]} />
-        </CardItem>
-        <CardItem style={styles.bottomSection}>
-          <TimeInfo timestamp={timestamp} />
-          <PedirBoton onSend={() => onSend(_stops, token)} disabled={loading} />
-        </CardItem>
-      </Card>
-    )
+        </View>
+      </CardItem>
+      <CardItem style={styles.locationContainer}>
+        <Location color={'#0000FF'} location={stops[0]} />
+        <Location color={'#33C534'} location={stops[stops.length - 1]} />
+      </CardItem>
+      <CardItem style={styles.bottomSection}>
+        <TimeInfo timestamp={timestamp} />
+        <PedirBoton onSend={() => onSend(_stops, token)} disabled={loading} />
+      </CardItem>
+    </Card>
+  )
 }
 
 ChooseTrip.propTypes = {

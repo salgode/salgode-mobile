c84a8227ab6687e16d11e0f440aa41d85c8005d3
Matias Andrade <mandrade2@uc.cl>
2019-10-23 17:11:36 -0300

    feat: fetch one trip connection

--- redux/actions/trips.js
+++ redux/actions/trips.js
@@ -2,6 +2,9 @@ export const actions = {
   TRIPS_FETCH_FUTURE_TRIPS: 'TRIPS/FETCH',
   TRIPS_FETCH_FUTURE_TRIPS_SUCCESS: 'TRIPS/FETCH_SUCCESS',
   TRIPS_FETCH_FUTURE_TRIPS_FAIL: 'TRIP/FETCH_FAIL',
+  TRIPS_FETCH_TRIP: 'TRIPS/FETCH_TRIP',
+  TRIPS_FETCH_TRIP_SUCCESS: 'TRIPS/FETCH_TRIP_SUCCESS',
+  TRIPS_FETCH_TRIP_FAIL: 'TRIP/FETCH_TRIP_FAIL',
 }
 
 export function fetchFutureTrips(authToken) {
@@ -10,6 +13,21 @@ export function fetchFutureTrips(authToken) {
     payload: {
       request: {
         url: `/trips/open`,
+        method: 'get',
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      },
+    },
+  }
+}
+
+export function fetchTrip(authToken, id) {
+  return {
+    type: actions.TRIPS_FETCH_FUTURE_TRIPS,
+    payload: {
+      request: {
+        url: `/trips/${id}`,
         method: 'get',
         headers: {
           Authorization: `Bearer ${authToken}`,

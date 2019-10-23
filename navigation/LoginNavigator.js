a12c248914c6b7d18b94b3ee9a8a074571876393
Dante <36767603+DanteMA@users.noreply.github.com>
2019-10-23 18:19:36 -0300

    feat/added-frontend-for-password-recovery (#74)
    it is NOT working

--- navigation/LoginNavigator.js
+++ navigation/LoginNavigator.js
@@ -2,11 +2,13 @@
 import { createStackNavigator } from 'react-navigation'
 import LoginScreen from '../screens/LoginScreen'
 import SignupScreen from '../screens/SignupScreen'
+import RecoverPasswordScreen from '../screens/RecoverPasswordScreen';
 
 export default createStackNavigator(
   {
     Login: LoginScreen,
     Signup: SignupScreen,
+    RecoverPassword: RecoverPasswordScreen, 
   },
   {
     navigationOptions: {

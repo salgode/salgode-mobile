b3b84e7fd58d7a7d7561d3c51076491b126e63d1
wayoalamos <malamos2@uc.cl>
2019-10-23 13:30:53 -0300

    remove nav details

--- components/Login/LoginForm.js
+++ components/Login/LoginForm.js
@@ -8,8 +8,8 @@ class LoginForm extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      email: 'example@mail.com',
-      password: 'password',
+      email: '',
+      password: '',
       inputValidity: true,
     }
 

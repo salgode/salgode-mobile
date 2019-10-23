ce80c2ce68b093ce9ccc93a38a0d8d76d6237857
Matias Andrade <mandrade2@uc.cl>
2019-10-23 17:11:36 -0300

    feat: fetch one trip connection

--- redux/actions/trips.js
+++ redux/actions/trips.js
@@ -1,41 +1,12 @@
 import { getBaseHeaders } from '../../config/api/headers'
 
 export const actions = {
-  TRIPS_FETCH_FUTURE_TRIPS: 'TRIPS_FETCH',
-  TRIPS_FETCH_FUTURE_TRIPS_SUCCESS: 'TRIPS_FETCH_SUCCESS',
-  TRIPS_FETCH_FUTURE_TRIPS_FAIL: 'TRIP_FETCH_FAIL',
-  TRIPS_FETCH_DRIVER_TRIPS: 'TRIPS_FETCH_DRIVER', //TODO: add real endpoint
-  TRIPS_FETCH_PAX_TRIPS: 'TRIPS_FETCH/PAX', //TODO: add real endpoint
-  TRIPS_FETCH_TRIP: 'TRIP_FETCH',
-  TRIPS_START_JOURNEY: 'TRIP_FETCH',
-  TRIPS_FETCH_PASSENGERS: 'TRIP_PASSENGERS',
-  TRIPS_GET_TRIP: 'TRIP_GET',
-}
-
-export function fetchFutureTripsByDriverId(authToken, driverId) {
-  return {
-    type: actions.TRIPS_FETCH_DRIVER_TRIPS,
-    payload: {
-      request: {
-        url: `/drivers/${driverId}/trips`, // TODO: add real url
-        method: 'get',
-        headers: getBaseHeaders(authToken),
-      },
-    },
-  }
-}
-
-export function fetchFutureTripsByPaxId(authToken, paxId) {
-  return {
-    type: actions.TRIPS_FETCH_PAX_TRIPS,
-    payload: {
-      request: {
-        url: `/paxs/${paxId}/trips`, // TODO: add real url
-        method: 'get',
-        headers: getBaseHeaders(authToken),
-      },
-    },
-  }
+  TRIPS_FETCH_FUTURE_TRIPS: 'TRIPS/FETCH',
+  TRIPS_FETCH_FUTURE_TRIPS_SUCCESS: 'TRIPS/FETCH_SUCCESS',
+  TRIPS_FETCH_FUTURE_TRIPS_FAIL: 'TRIP/FETCH_FAIL',
+  TRIPS_FETCH_TRIP: 'TRIPS/FETCH_TRIP',
+  TRIPS_FETCH_TRIP_SUCCESS: 'TRIPS/FETCH_TRIP_SUCCESS',
+  TRIPS_FETCH_TRIP_FAIL: 'TRIP/FETCH_TRIP_FAIL',
 }
 
 export function fetchFutureTrips(authToken) {
@@ -67,27 +38,16 @@ export function startJourney(authToken, tripId, users) {
   }
 }
 
-export function fetchPassengers(authToken, tripId) {
+export function fetchTrip(authToken, id) {
   return {
-    type: actions.TRIPS_FETCH_PASSENGERS,
+    type: actions.TRIPS_FETCH_FUTURE_TRIPS,
     payload: {
       request: {
-        url: `/trips/${tripId}/slots`,
+        url: `/trips/${id}`,
         method: 'get',
-        headers: getBaseHeaders(authToken),
-      },
-    },
-  }
-}
-
-export function getTrip(authToken, tripId) {
-  return {
-    type: actions.TRIPS_GET_TRIP,
-    payload: {
-      request: {
-        url: `/trips/${tripId}`,
-        method: 'get',
-        headers: getBaseHeaders(authToken),
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
       },
     },
   }

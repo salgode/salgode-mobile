5d901774f236f7dc5aa08b4527261695efe10d93
Benjamin Earle <benjaminearle123@gmail.com>
2019-10-25 12:43:33 -0300

    WIP

--- components/Trips/TripRequest.js
+++ components/Trips/TripRequest.js
@@ -36,7 +36,7 @@ const TripRequest = ({ stops, onSend, loading }) => {
         <Button
           disabled={!state.selected}
           style={state.selected ? styles.button : styles.unselectedButton}
-          onPress={() => onSend(stops[stops.length - 1], state.selectedStop)}
+          onPress={() => onSend(state.selectedStop, stops[stops.length - 1])}
         >
           <Text>Confirmar Solicitud</Text>
         </Button>

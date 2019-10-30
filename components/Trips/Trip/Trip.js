f3541f73fa6b679359ad759ab64e78b9f5ec2ecc
Matias Andrade <mandrade2@uc.cl>
2019-10-30 17:14:02 -0300

    feat: drivinf trip status

--- components/Trips/Trip/Trip.js
+++ components/Trips/Trip/Trip.js
@@ -18,7 +18,21 @@ export const Trip = ({
   trip,
 }) => {
   if (trip.trip_status === 'completed') {
-    return <View />
+    return null
+  }
+  const status = trip.trip_status
+  let statusColor
+  let statusText
+
+  if (status === 'open') {
+    statusColor = 'green'
+    statusText = 'Por iniciar'
+  } else if (status === 'in_progress') {
+    statusColor = 'purple'
+    statusText = 'En curso'
+  } else {
+    statusColor = 'blue'
+    statusText = 'Completado'
   }
   return (
     <TouchableOpacity
@@ -28,6 +42,9 @@ export const Trip = ({
     >
       <Card style={[styles.container, styles.shadow]}>
         <View style={styles.locationContainer}>
+          <View style={{ ...styles.status, backgroundColor: statusColor }}>
+            <Text style={styles.statusText}>{statusText}</Text>
+          </View>
           <Text>{user.driver_name}</Text>
           <Location color={'#0000FF'} location={startLocation.place_name} />
           <Location color={'#33C534'} location={endLocation.place_name} />
@@ -96,6 +113,8 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.5,
     shadowRadius: 5,
   },
+  status: { borderRadius: 15, paddingHorizontal: 10, paddingVertical: 2 },
+  statusText: { color: 'white', fontSize: 12, fontWeight: '700' },
   // spacesUsed: {
   //   alignSelf: 'center',
   //   color: Colors.textGray,

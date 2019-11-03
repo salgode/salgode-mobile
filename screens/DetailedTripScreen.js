99c47af7f0c98e85a524dfe8f9dc44da0981f413
Matias Andrade <mandrade2@uc.cl>
2019-11-03 01:23:23 -0300

    feat: accept reservations only if trip open

--- screens/DetailedTripScreen.js
+++ screens/DetailedTripScreen.js
@@ -1,73 +1,73 @@
-import React, { Component } from 'react'
-import { StyleSheet } from 'react-native'
-import PropTypes from 'prop-types'
-import DetailedTrip from '../components/Trips/Trip/DetailedTrip'
-import { Spinner } from 'native-base'
-import TripRequestCard from '../components/Trips/Trip/TripRequestCard'
-import { ScrollView } from 'react-native-gesture-handler'
-import { connect } from 'react-redux'
-import { withNavigation } from 'react-navigation'
+import React, { Component } from "react";
+import { StyleSheet } from "react-native";
+import PropTypes from "prop-types";
+import DetailedTrip from "../components/Trips/Trip/DetailedTrip";
+import { Spinner } from "native-base";
+import TripRequestCard from "../components/Trips/Trip/TripRequestCard";
+import { ScrollView } from "react-native-gesture-handler";
+import { connect } from "react-redux";
+import { withNavigation } from "react-navigation";
 import {
   fetchTrip,
   startJourney,
-  fetchTripManifest,
-} from '../redux/actions/trips'
-import { getTripReservations } from '../utils/getTripInfo'
+  fetchTripManifest
+} from "../redux/actions/trips";
+import { getTripReservations } from "../utils/getTripInfo";
 
 class DetailedTripScreen extends Component {
   static navigationOptions = {
-    title: 'Mis Viajes',
-  }
+    title: "Mis Viajes"
+  };
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       loading: true,
       trip: null,
       reservations: [],
-      asDriver: false,
-    }
+      asDriver: false
+    };
 
-    this.renderPassengers = this.renderPassengers.bind(this)
-    this.onPressStartTrip = this.onPressStartTrip.bind(this)
+    this.renderPassengers = this.renderPassengers.bind(this);
+    this.onPressStartTrip = this.onPressStartTrip.bind(this);
   }
 
   async componentDidMount() {
-    this.setState({ loading: true })
-    const asDriver = this.props.navigation.getParam('asDriver', null)
-    const trip_id = this.props.navigation.getParam('trip_id', null)
-    this.props.fetchTrip(this.props.user.token, trip_id)
-    this.props.fetchManifest(this.props.user.token, trip_id)
-    this.setState({ ...this.state, asDriver })
+    this.setState({ loading: true });
+    const asDriver = this.props.navigation.getParam("asDriver", null);
+    const trip_id = this.props.navigation.getParam("trip_id", null);
+    this.props.fetchTrip(this.props.user.token, trip_id);
+    this.props.fetchManifest(this.props.user.token, trip_id);
+    this.setState({ ...this.state, asDriver });
 
     if (asDriver) {
       const reservations = await getTripReservations(
         this.props.user.token,
         trip_id
-      )
-      this.setState({ loading: false })
+      );
+      this.setState({ loading: false });
       if (!reservations || reservations.message) {
         alert(
-          'Hubo un problema obteniendo las reservas. Por favor intentalo de nuevo.'
-        )
+          "Hubo un problema obteniendo las reservas. Por favor intentalo de nuevo."
+        );
       } else {
-        this.setState({ reservations })
+        this.setState({ reservations });
       }
     } else {
-      this.setState({ loading: false })
+      this.setState({ loading: false });
     }
   }
   static getDerivedStateFromProps(nextProps, prevState) {
     if (nextProps.trip !== prevState.trip) {
-      return { trip: nextProps.trip }
+      return { trip: nextProps.trip };
     }
     if (nextProps.trip.manifest !== prevState.trip.manifest) {
-      return { trip: { ...prevState.trip, manifest: nextProps.trip.manifest } }
+      return { trip: { ...prevState.trip, manifest: nextProps.trip.manifest } };
     }
-    return null
+    return null;
   }
   onPressStartTrip() {
-    this.props.navigation.navigate('StartTrip', {
+    this.props.navigation.navigate("StartTrip", {
       stops: this.state.trip.trip_route_points,
       onTripStart: () =>
         this.props.postTripStart(
@@ -75,21 +75,21 @@ class DetailedTripScreen extends Component {
           this.state.trip.trip_id
         ),
       nextTripView: () => {
-        this.props.navigation.navigate('StopTrip', {
+        this.props.navigation.navigate("StopTrip", {
           token: this.props.user.token,
           trip: this.state.trip,
-          asDriver: this.props.navigation.getParam('asDriver', null),
-        })
-      },
-    })
+          asDriver: this.props.navigation.getParam("asDriver", null)
+        });
+      }
+    });
   }
 
   renderPassengers() {
     if (!this.state.trip) {
-      return null
+      return null;
     }
 
-    const finishStop = this.state.trip.trip_route.end.name
+    const finishStop = this.state.trip.trip_route.end.name;
     return this.state.reservations.length > 0
       ? this.state.reservations.map((reservation, index) => {
           return (
@@ -98,10 +98,11 @@ class DetailedTripScreen extends Component {
               reservation={reservation}
               finishStop={finishStop}
               token={this.props.user.token}
+              tripStatus={this.state.trip.trip_status}
             />
-          )
+          );
         })
-      : null
+      : null;
   }
 
   render() {
@@ -120,7 +121,7 @@ class DetailedTripScreen extends Component {
           ? this.renderPassengers()
           : null}
       </ScrollView>
-    )
+    );
   }
 }
 
@@ -129,42 +130,42 @@ DetailedTripScreen.propTypes = {
   tripId: PropTypes.string,
   navigation: PropTypes.shape({
     navigate: PropTypes.func.isRequired,
-    getParam: PropTypes.func.isRequired,
+    getParam: PropTypes.func.isRequired
   }).isRequired,
   user: PropTypes.shape({
-    token: PropTypes.string.isRequired,
+    token: PropTypes.string.isRequired
   }).isRequired,
   trip: PropTypes.object.isRequired,
   fetchTrip: PropTypes.func.isRequired,
   fetchManifest: PropTypes.func.isRequired,
-  postTripStart: PropTypes.func.isRequired,
-}
+  postTripStart: PropTypes.func.isRequired
+};
 
 DetailedTripScreen.defaultProps = {
   asDriver: false,
-  tripId: '0',
-}
+  tripId: "0"
+};
 
 const styles = StyleSheet.create({
   container: {
     padding: 15,
-    ...StyleSheet.absoluteFill,
-  },
-})
+    ...StyleSheet.absoluteFill
+  }
+});
 
 const mapPropsToState = state => ({
   user: state.user,
   trip: state.trips.trip,
-  manifest: state.trips.trip.manifest,
-})
+  manifest: state.trips.trip.manifest
+});
 
 const mapDispatchToState = dispatch => ({
   fetchTrip: (token, id) => dispatch(fetchTrip(token, id)),
   postTripStart: (token, id) => dispatch(startJourney(token, id)),
-  fetchManifest: (token, id) => dispatch(fetchTripManifest(token, id)),
-})
+  fetchManifest: (token, id) => dispatch(fetchTripManifest(token, id))
+});
 
 export default connect(
   mapPropsToState,
   mapDispatchToState
-)(withNavigation(DetailedTripScreen))
+)(withNavigation(DetailedTripScreen));

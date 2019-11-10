be28df84d213b30bd6986abc14ccd2baada5c7a7
Matias Andrade <mandrade2@uc.cl>
2019-11-10 01:14:08 -0300

    feat: add to navigators

--- navigation/LoginNavigator.js
+++ navigation/LoginNavigator.js
@@ -2,6 +2,8 @@ import { createStackNavigator } from 'react-navigation'
 
 import { LoginScreen, RecoverPasswordScreen, SignupScreen } from '../screens'
 import ImageSignupForm from '../components/Login/ImageSignupForm'
+import TermScreen from '../screens/TermScreen'
+import PrivacyScreen from '../screens/PrivacyScreen'
 
 export default createStackNavigator(
   {
@@ -9,6 +11,8 @@ export default createStackNavigator(
     Signup: SignupScreen,
     SignupImages: ImageSignupForm,
     RecoverPassword: RecoverPasswordScreen,
+    Terms: TermScreen,
+    Privacy: PrivacyScreen,
   },
   {
     navigationOptions: {

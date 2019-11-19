997b0617797d55dd43a7af6c76d49b72551e020b
Benjamin Earle <benjaminearle123@gmail.com>
2019-11-18 23:37:32 -0300

    Added redirection on notification press

--- utils/notifications.js
+++ utils/notifications.js
@@ -1,8 +1,10 @@
 import * as Permissions from 'expo-permissions'
 import { Notifications } from 'expo'
+import Constants from 'expo-constants'
 import { AsyncStorage } from 'react-native'
 import { getOwnProfile } from '../redux/actions/user'
 import { dispatch } from 'react-redux'
+import userModel from '../redux/models/user'
 
 export const registerForPushNotifications = async () => {
   const { status: existingStatus } = await Permissions.getAsync(
@@ -24,13 +26,26 @@ export const registerForPushNotifications = async () => {
     return null
   }
 
-  return Notifications.getExpoPushTokenAsync()
+  if (Constants.isDevice) {
+    return Notifications.getExpoPushTokenAsync()
+  }
 }
 
 const dispatchNavigation = (navigation, notificationData) => {
-  const { action, resource } = notificationData
+  const { action, resource, resource_id } = notificationData
   if (action === 'decline' && resource === 'trip') {
     navigation.navigate('Pedidos')
+  } else if (action === 'request' && resource === 'trip' && resource_id) {
+    navigation.navigate('DetailedTrip', {
+      asDriver: true,
+      trip_id: resource_id,
+    })
+  } else if (action === 'accept' && resource === 'trip' && resource_id) {
+    // TODO
+    navigation.navigate('ReservationDetails', {
+      userData: null,
+      vehicle: null, // TODO: Ver como llegan a RequestedTrip.js
+    })
   }
 }
 
@@ -40,9 +55,11 @@ export const handleNotification = navigation => {
     const userToken = await AsyncStorage.getItem('@userToken').then(JSON.parse)
     const userId = await AsyncStorage.getItem('@userId').then(JSON.parse)
     if (!userToken || !userId) return
-    if (notification.origin === 'selected' && notification.data.length) {
-      const response = await dispatch(getOwnProfile(userToken))
-      if (!response || response.error) return
+    if (notification.origin === 'selected') {
+      if (!userModel.email) {
+        const response = await dispatch(getOwnProfile(userToken))
+        if (!response || response.error) return
+      }
       // TODO: Navegar
       dispatchNavigation(navigation, notification.data)
     }

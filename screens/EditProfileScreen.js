4b87e7bc586e0f52cd0d9815fb7a1b25eaf4947e
Benjamin Earle <benjaminearle123@gmail.com>
2019-11-22 13:29:25 -0300

    :bug: Readded missing import

--- screens/EditProfileScreen.js
+++ screens/EditProfileScreen.js
@@ -58,6 +58,7 @@ import { uploadImageToS3 } from '../utils/image'
 import * as ImagePicker from 'expo-image-picker'
 import { analytics, ANALYTICS_CATEGORIES } from '../utils/analytics'
 import Divider from '../components/Divider'
+import { registerForPushNotifications } from '../utils/notifications'
 
 function validateName(str) {
   if (typeof str !== 'string') {
